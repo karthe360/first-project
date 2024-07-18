7180038a848113c8f2005e0a3774c5ffc4bfc647
karthe360 <karthegetu@gmail.com>
2024-07-18 18:19:27 +0530

    project_car.py

--- add.py
+++ add.py
@@ -1,4 +1,17 @@
-a=20
-b=10
-c=(a+b)
-print(c)
+class car:
+    def __init__(self,car_rank,car_name,car_id):
+        self.car_rank=car_rank
+        self.car_name=car_name
+        self.car_id=car_id
+    def car_bio(self):
+
+        print("The ranking of car :",self.car_rank,self.car_name,self.car_id)
+
+
+hifi_cars1=car("FIRST","JAGUAR",9999)
+hifi_cars2=car("SECOND","FERRARI",3535)
+hifi_cars3=car("THIRD","BMW",3333)
+print("###..welcome to car mania..###")
+hifi_cars1.car_bio()
+hifi_cars2.car_bio()
+hifi_cars3.car_bio()
